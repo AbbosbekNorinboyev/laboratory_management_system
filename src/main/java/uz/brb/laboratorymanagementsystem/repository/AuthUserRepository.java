81f918bf9ccc5d4ab7041e150967a9cf73eba151
AbbosbekNorinboyev <norinboyevabboskhan002@gmail.com>
2026-04-26 15:13:47 +0500

    fix

--- src/main/java/uz/brb/laboratorymanagementsystem/repository/AuthUserRepository.java
+++ src/main/java/uz/brb/laboratorymanagementsystem/repository/AuthUserRepository.java
@@ -1,7 +1,6 @@
 package uz.brb.laboratorymanagementsystem.repository;
 
 import jakarta.persistence.Tuple;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
